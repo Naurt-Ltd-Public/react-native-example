24342d47e29ac76db8a7f423fdf4dbbe82a62e74
NickS <nick.s@naurt.com>
2022-02-25 10:31:36 +0000

    feat: Added bare-bones example inside the NaurtAndroidComponent

--- NaurtAndroidComponent.tsx
+++ NaurtAndroidComponent.tsx
@@ -1,8 +1,27 @@
-import React from "react"
+import React, { useEffect } from "react"
 import NaurtAndroidModule from "./NaurtModule"
 
 const NaurtAndroidComponent = () => {
     const naurtAndroidConstants = NaurtAndroidModule.getConstants();
+
+    useEffect(() => {
+        let hasPermission = NaurtAndroidModule.checkPermissions();
+
+        if (hasPermission) {
+            NaurtAndroidModule.initialiseNaurt('<YOUR_API_KEY_HERE>')
+        }
+
+        // Simple Start here, post initialisation
+        NaurtAndroidModule.startNaurt((eid, lat, lon, time) => {
+            if (eid === Number(naurtAndroidConstants.get("NAURT_EVENT_ID"))) {
+                console.log(`New Naurt Location!: ${lat}, ${lon} at ${time}`)
+            }
+        })
+    
+        return () => {
+            NaurtAndroidModule.stopNaurt()
+        }
+    }, [])
 
     return (
         <></>
