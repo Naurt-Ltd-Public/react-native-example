dcdef7410cfe1fd437147b034af5106aad37850f
Aivaras Vaisnoras <aiva.v@naurt.com>
2022-03-07 14:04:39 +0000

    feat: initial xcode integration & module usage

---
+++ ios/RNNaurtIosModule.swift
@@ -0,0 +1,14 @@
+//
+//  RNNaurtIosModule.swift
+//  naurt_react_native_typescript
+//
+//  Created by Bob on 07/03/2022.
+//
+
+import Foundation
+import Naurt_SDK
+
+@objc(RNNaurtIosModule)
+class RNNaurtIosModule: NSObject {
+  
+}
