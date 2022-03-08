c5596a8b96d457e398eae0509b5302f65e8ab3bc
Aivaras Vaisnoras <aiva.v@naurt.com>
2022-03-08 11:08:12 +0000

    feat: initial react native wrapper

--- ios/RNNaurtIosModule.swift
+++ ios/RNNaurtIosModule.swift
@@ -10,5 +10,33 @@ import Naurt_SDK
 
 @objc(RNNaurtIosModule)
 class RNNaurtIosModule: NSObject {
+  @objc
+  static func requiresMainQueueSetup() -> Bool {
+    return true
+  }
   
+  @objc
+  static func initialiseNaurt(apiKey: String, precision: Int) {
+    Naurt.shared.initialise(apiKey: apiKey, precision: precision);
+  }
+  
+  @objc
+  static func startNaurt() {
+    Naurt.shared.start()
+  }
+  
+  @objc
+  static func stopNaurt() {
+    Naurt.shared.stop()
+  }
+  
+  @objc
+  static func pauseNaurt() {
+    Naurt.shared.pause()
+  }
+  
+  @objc
+  static func resumeNaurt() {
+    Naurt.shared.resume()
+  }
 }
